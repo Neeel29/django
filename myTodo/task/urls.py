78b17990e46ffcda707912f3c49db4b0610d38bf
Neel <neel.gorasiya@fxis.ai>
2025-01-22 16:02:43 +0530

    added update and delete requests

--- myTodo/task/urls.py
+++ myTodo/task/urls.py
@@ -2,5 +2,6 @@ from task import views
 from django.urls import path
 
 urlpatterns = [
-    path('task', views.task_list, name='task_list'),
+    path('', views.task_list, name='tasks_list_or_create'),
+    path('<int:pk>', views.task_action, name='task_update_or_delete'),
 ]
--- myTodo/task/views.py
+++ myTodo/task/views.py
@@ -8,7 +8,9 @@ from .models import *
 def task_list(request):
     # List all tasks or create a new task
     if request.method == 'GET':
-        tasks = Task.objects.all()
+        if request.GET.get('id'):
+            tasks = Task.objects.filter(id=request.GET.get('id'))
+        else: tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
@@ -17,3 +19,20 @@ def task_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def task_action(request, pk):
+    # Update or delete a task
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'PUT':
+        serializer = TaskSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
